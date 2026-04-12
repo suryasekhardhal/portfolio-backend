beda1ef487234f489bc9f27d0df76d64ffdc9d39
Suryasekhar Dhal <suryasekhardhal2003@gmail.com>
2026-04-13 02:03:33 +0530

    fix: update email sending logic in contact controller to include message data

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -8,10 +8,9 @@ export const contact = asyncHandler(async (req, res) => {
   if (!name || !email || !message) {
     throw new ApiError(400, "All fields are required");
   }
-  const emailSubject = `New contact message from ${name}`;
-  const emailText = `You have received a new message from the contact form:\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`;
-  await sendEmail(process.env.EMAIL_USER, emailSubject, emailText);
+  
+  const data = await sendEmail({ name, email, message });
   res
     .status(200)
-    .json(new ApiResponse(200, null, "Message sent successfully"));
+    .json(new ApiResponse(200, data, "Message sent successfully"));
 });
